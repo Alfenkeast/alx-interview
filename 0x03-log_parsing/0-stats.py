d1b07cecda5f3297af6dc46d46c82863cdd49ac7
Patience <alfredpatience03@gmail.com>
2023-07-24 14:49:42 +0100

    log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,15 +1,40 @@
 #!/usr/bin/python3
-import random
+""" doc """
 import sys
-from time import sleep
-import datetime
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+
+if __name__ == "__main__":
+    i = 0
+    status = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+    }
+    fileSize = 0
+
+    def printstats(fileSize, status):
+        """ doc """
+        print("File size: {:d}".format(fileSize))
+        for key in sorted(status.keys()):
+            if status[key] != 0:
+                print("{}: {:d}".format(key, status[key]))
+
+    try:
+        for line in sys.stdin:
+            words = line.split()
+            if len(words) >= 2:
+                if words[-2] in status.keys():
+                    status[words[-2]] += 1
+                fileSize += int(words[-1])
+                i += 1
+                if not i % 10:
+                    printstats(fileSize, status)
+        printstats(fileSize, status)
+    except KeyboardInterrupt:
+        printstats(fileSize, status)
+        raise
