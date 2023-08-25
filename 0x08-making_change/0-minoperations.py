429ee0e97b35dbf6c18c6364fb7018f9a3ffa656
Alfenkeast <alfredpatience03@gmail.com>
2023-08-25 08:10:39 -0700

    making_change

---
+++ 0x08-making_change/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
+
+
+coins = [1, 2, 5]
+total = 11
+result = makeChange(coins, total)
+print(result)
