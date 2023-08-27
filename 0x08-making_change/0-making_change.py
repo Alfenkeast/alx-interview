2beabf07913c43c51dbf5396d09deade63f270e1
Alfenkeast <alfredpatience03@gmail.com>
2023-08-27 06:07:17 -0700

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,14 +2,14 @@
 
 def makeChange(coins, total):
     if total <= 0:
-        return 0
+        return -1
 
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
     for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
 
     if dp[total] == floar('inf'):
         return -1
