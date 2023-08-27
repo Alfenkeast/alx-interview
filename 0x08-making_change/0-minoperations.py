9693a56e529ef424a1c5d0f49d06cfbb1e1ccbf8
Alfenkeast <alfredpatience03@gmail.com>
2023-08-27 05:04:25 -0700

    0-minoperations.py

--- 0x08-making_change/0-minoperations.py
+++ 0x08-making_change/0-minoperations.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
 """ Script that computes a minimum operations
-    needed in a CopyAll - Paste task
 """
 
 
 def minOperations(n):
-    """
-    Method for compute the minimum number
-    of operations for task Copy All and Paste
-
-    Args:
-        n: input value
-        factor_list: List to save the operations
-    Return: the sum of the operations
-    """
     if n < 2:
         return 0
     factor_list = []
